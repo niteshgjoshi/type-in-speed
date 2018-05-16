f79f7b2f1477a402101cba368cd26674579e0492
Nitesh <niteshgjoshi@gmail.com>
2018-05-16 01:52:51 -0700

    Finalized JS

--- script.js
+++ script.js
@@ -3,53 +3,72 @@ const testArea = document.querySelector("#test-area");
 const originText = document.querySelector("#origin-text p").innerHTML;
 const resetButton = document.querySelector("#reset");
 const theTimer = document.querySelector(".timer");
-var sec = 0;
+var timer = [0,0,0,0];
 var min = 0;
 var hr = 0;
 
 // Add leading zero to numbers 9 or below (purely for aesthetics):
-
+function leadingZero(time){
+	if(time<=9)
+		time = '0'+time;
+	return time;
+}
 
 // Run a standard minute/second/hundredths timer:
-var runTimer;
+function runTimer(){
+	timer[3]++;
+	timer[0] = Math.floor((timer[3]/100)/60);
+	timer[1] = Math.floor((timer[3]/100))%60;
+	timer[2] = timer[3]%100;
+
+	var timeStr = leadingZero(timer[0])+":"+leadingZero(timer[1])+":"+leadingZero(timer[2]);
+	theTimer.innerHTML = timeStr;
+}
+
+// Match the text entered with the provided text on the page:
+function matchStr(){
+	var enteredText=testArea.value;
+	var originTextMatch = originText.substring(0,enteredText.length);
+
+	if(enteredText===originText)
+	{
+		testWrapper.style.borderColor = "green";
+		clearInterval(timerVar);
+	}
+	else{
+		if(enteredText==originTextMatch)
+			testWrapper.style.borderColor = "#65CCf3";
+		else
+			testWrapper.style.borderColor = "#E95D0F";	
+	}
+
+}
+
+// Start the timer:
+var timerVar;
 var first = true;
 function startTimer(){
 
 	console.log("yo");
 
 	if(first){
-		runTimer = setInterval(unanmed,1000);
+		timerVar = setInterval(runTimer,10);
 		first=false;
 	}
 }
 
-function unanmed(){
-	sec = sec + 1;
-	if(sec==60)
-	{
-		sec=0;
-		min = min + 1;
-	}
-	if(min==60)
-	{
-		min=0;
-		hr = hr + 1;
-	}
-	theTimer.innerHTML = hr+":"+min+":"+sec;
-}
-// Match the text entered with the provided text on the page:
-
-
-// Start the timer:
-
-
 // Reset everything:
 function resetAll(){
-	clearInterval(runTimer);
+	clearInterval(timerVar);
+	timerVar = null;
 	theTimer.innerHTML = "00:00:00";
-	testArea.innerHTML = "The clock starts Farting";
+	testArea.value = "";
+	first = true;
+	timer = [0,0,0,0];
+	testWrapper.style.borderColor = "grey";
 }
 
 // Event listeners for keyboard input and the reset button:
 testArea.addEventListener("keypress", startTimer,false);
+testArea.addEventListener("keyup", matchStr,false);
 resetButton.addEventListener("click", resetAll,false)
